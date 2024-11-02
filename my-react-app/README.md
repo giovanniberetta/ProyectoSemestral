0aa31e8e19dfadb486175fe12090fd4ff8d40cfc
ZEKKENoz <145718542+ZEKKENoz@users.noreply.github.com>
2024-11-02 15:07:40 -0300

    corregir final y quite imagenes
    para ver si corre le quite las images
    a los css, pero no se ejecuta y va lentito en el siguiente commit veré si puedo corregir.

--- my-react-app/README.md
+++ my-react-app/README.md
@@ -71,3 +71,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 
 npm install react-scripts --save
 npm install react-router-dom
+npm install react-router-dom --legacy-peer-deps
--- my-react-app/src/components/home/Home.jsx
+++ my-react-app/src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import Hblog from "./Hblog"
 import HAbout from "./HAbout"
 import Hero from "./hero/Hero"
 import Hprice from "./Hprice"
-import Testimonal from "./testimonal/Testimonal"
+import Testimonal from "./testimonial/Testimonial"
 
 const Home = () => {
   return (

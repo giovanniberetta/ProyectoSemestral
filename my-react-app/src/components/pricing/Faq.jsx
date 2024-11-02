27f84a64feda6ba5219b31050bdfd9ab2f784b22
ZEKKENoz <145718542+ZEKKENoz@users.noreply.github.com>
2024-11-02 15:53:12 -0300

    ahora corre
    se corrigió el fallo se comentó en el App.js las rutas a archivos con errores

--- my-react-app/src/components/pricing/Faq.jsx
+++ my-react-app/src/components/pricing/Faq.jsx
@@ -1,39 +1,45 @@
-import React, { useRef, useState } from "react"
-import { faq } from "../../dummydata"
-import Heading from "../common/heading/Heading"
+import React, { useState } from "react";
+import { faq } from "../../dummydata";
+import Heading from "../common/heading/Heading";
 
 const Faq = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
 
   const toggle = (index) => {
     if (click === index) {
-      return setClick(null)
+      return setClick(null);
     }
-    setClick(index)
-  }
+    setClick(index);
+  };
 
   return (
     <>
-      <Heading subtitle='FAQS' title='Frequesntly Ask Question' />
-      <section className='faq'>
-        <div className='container'>
+      <Heading subtitle="FAQS" title="Frequently Asked Questions" />
+      <section className="faq">
+        <div className="container">
           {faq.map((val, index) => (
-            <div className='box'>
-              <button className='accordion' onClick={() => toggle(index)} key={index}>
+            <div className="box" key={index}>
+              <button className="accordion" onClick={() => toggle(index)}>
                 <h2>{val.title}</h2>
-                <span>{click === index ? <i className='fa fa-chevron-down'></i> : <i className='fa fa-chevron-right'></i>}</span>
+                <span>
+                  {click === index ? (
+                    <i className="fa fa-chevron-down"></i>
+                  ) : (
+                    <i className="fa fa-chevron-right"></i>
+                  )}
+                </span>
               </button>
-              {click === index ? (
-                <div className='text'>
+              {click === index && (
+                <div className="text">
                   <p>{val.desc}</p>
                 </div>
-              ) : null}
+              )}
             </div>
           ))}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Faq
+export default Faq;

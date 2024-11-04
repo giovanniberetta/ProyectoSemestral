a7ee3d50e110754d1b5ab357eebbbc4508735ba7
KhalashXD <130528544+KhalashXD@users.noreply.github.com>
2024-11-03 22:41:55 -0300

    textos

--- my-react-app/src/components/contact/Contact.jsx
+++ my-react-app/src/components/contact/Contact.jsx
@@ -10,6 +10,7 @@ const Contact = () => {
       <section className='contacts padding'>
         <div className='container shadow flexSB'>
           <div className='left row'>
+             <img src="../../../../public/images/contact.jpg" alt="Descripción de la imagen" />
           </div>
           <div className='right row'>
             <h1>Contact us</h1>
@@ -18,15 +19,15 @@ const Contact = () => {
             <div className='items grid2'>
               <div className='box'>
                 <h4>ADDRESS:</h4>
-                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                <p>Av. Apoquindo 2827, 7550268 Las Condes, Santiago, Chile.</p>
               </div>
               <div className='box'>
                 <h4>EMAIL:</h4>
-                <p> info@yoursite.com</p>
+                <p> contacto@nosequenombreponer.com</p>
               </div>
               <div className='box'>
                 <h4>PHONE:</h4>
-                <p> + 1235 2355 98</p>
+                <p> +56 2 600 9000</p>
               </div>
             </div>
 
@@ -43,7 +44,7 @@ const Contact = () => {
             </form>
 
             <h3>Follow us here</h3>
-            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
+            <span>FACEBOOK TWITTER INSTAGRAM LINKDIN</span>
           </div>
         </div>
       </section>
